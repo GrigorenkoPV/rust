10d18de7f3a4d5d03b6d83bd4e7732b3494482d9
Pavel Grigorenko <GrigorenkoPV@ya.ru>
2026-03-29 22:20:45 +0300

    `vec::ExtractIf`: reduce the severity of leak amplification

--- library/alloc/src/vec/extract_if.rs
+++ library/alloc/src/vec/extract_if.rs
@@ -24,13 +24,13 @@ pub struct ExtractIf<
     F,
     #[unstable(feature = "allocator_api", issue = "32838")] A: Allocator = Global,
 > {
-    vec: &'a mut Vec<T, A>,
-    /// The index of the item that will be inspected by the next call to `next`.
-    idx: usize,
+    valid_prefix: &'a mut Vec<T, A>,
+
+    /// The number of items that have been drained (removed) thus far.
+    hole_size: usize,
+
     /// Elements at and beyond this point will be retained. Must be equal or smaller than `old_len`.
     end: usize,
-    /// The number of items that have been drained (removed) thus far.
-    del: usize,
     /// The original length of `vec` prior to draining.
     old_len: usize,
     /// The filter test predicate.
@@ -43,17 +43,15 @@ impl<'a, T, F, A: Allocator> ExtractIf<'a, T, F, A> {
         let Range { start, end } = slice::range(range, ..old_len);
 
         // Guard against the vec getting leaked (leak amplification)
-        unsafe {
-            vec.set_len(0);
-        }
-        ExtractIf { vec, idx: start, del: 0, end, old_len, pred }
+        unsafe { vec.set_len(start) };
+        ExtractIf { valid_prefix: vec, hole_size: 0, end, old_len, pred }
     }
 
     /// Returns a reference to the underlying allocator.
     #[unstable(feature = "allocator_api", issue = "32838")]
     #[inline]
     pub fn allocator(&self) -> &A {
-        self.vec.allocator()
+        self.valid_prefix.allocator()
     }
 }
 
@@ -65,60 +63,54 @@ where
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
-        while self.idx < self.end {
-            let i = self.idx;
-            // SAFETY:
-            //  We know that `i < self.end` from the if guard and that `self.end <= self.old_len` from
-            //  the validity of `Self`. Therefore `i` points to an element within `vec`.
-            //
-            //  Additionally, the i-th element is valid because each element is visited at most once
-            //  and it is the first time we access vec[i].
-            //
-            //  Note: we can't use `vec.get_unchecked_mut(i)` here since the precondition for that
-            //  function is that i < vec.len(), but we've set vec's length to zero.
-            let cur = unsafe { &mut *self.vec.as_mut_ptr().add(i) };
-            let drained = (self.pred)(cur);
-            // Update the index *after* the predicate is called. If the index
-            // is updated prior and the predicate panics, the element at this
-            // index would be leaked.
-            self.idx += 1;
-            if drained {
-                self.del += 1;
-                // SAFETY: We never touch this element again after returning it.
-                return Some(unsafe { ptr::read(cur) });
-            } else if self.del > 0 {
-                // SAFETY: `self.del` > 0, so the hole slot must not overlap with current element.
-                // We use copy for move, and never touch this element again.
+        loop {
+            let valid_prefix_len = self.valid_prefix.len();
+            let hole_size = self.hole_size;
+            let start = self.valid_prefix.as_mut_ptr();
+            let hole = unsafe { start.add(valid_prefix_len) };
+            let tail = unsafe { hole.add(hole_size) };
+
+            if (self.pred)(
                 unsafe {
-                    let hole_slot = self.vec.as_mut_ptr().add(i - self.del);
-                    ptr::copy_nonoverlapping(cur, hole_slot, 1);
+                    slice::from_raw_parts_mut(tail, self.end - (hole_size + valid_prefix_len))
                 }
+                .first_mut()?,
+            ) {
+                self.hole_size += 1;
+                // SAFETY: We never touch this element again after returning it.
+                return Some(unsafe { ptr::read(tail) });
+            } else {
+                if self.hole_size > 0 {
+                    unsafe { ptr::copy_nonoverlapping(tail, hole, 1) };
+                }
+                unsafe { self.valid_prefix.set_len(valid_prefix_len + 1) };
             }
         }
-        None
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (0, Some(self.end - self.idx))
+        (0, Some(self.end - self.valid_prefix.len() - self.hole_size))
     }
 }
 
 #[stable(feature = "extract_if", since = "1.87.0")]
 impl<T, F, A: Allocator> Drop for ExtractIf<'_, T, F, A> {
     fn drop(&mut self) {
-        if self.del > 0 {
+        let hole_size = self.hole_size;
+        if hole_size > 0 {
+            let valid_prefix_len = self.valid_prefix.len();
+            let valid_tail_len = self.old_len - valid_prefix_len - hole_size;
+            let start = self.valid_prefix.as_mut_ptr();
+
             // SAFETY: Trailing unchecked items must be valid since we never touch them.
             unsafe {
-                ptr::copy(
-                    self.vec.as_ptr().add(self.idx),
-                    self.vec.as_mut_ptr().add(self.idx - self.del),
-                    self.old_len - self.idx,
-                );
+                let hole = start.add(valid_prefix_len);
+                ptr::copy(hole.add(hole_size), hole, valid_tail_len);
             }
         }
         // SAFETY: After filling holes, all items are in contiguous memory.
         unsafe {
-            self.vec.set_len(self.old_len - self.del);
+            self.valid_prefix.set_len(self.old_len - self.hole_size);
         }
     }
 }
@@ -132,18 +124,17 @@ where
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         // We have to use pointer arithmetics here,
         // because the length of `self.vec` is temporarily set to 0.
-        let start = self.vec.as_ptr();
+        let start = self.valid_prefix.as_ptr();
 
-        // SAFETY: we always keep first `self.idx - self.del` elements valid.
-        let retained = unsafe { slice::from_raw_parts(start, self.idx - self.del) };
+        let retained = self.valid_prefix.as_slice();
 
-        // SAFETY: we have not yet touched elements starting at `self.idx`.
+        let tail_start = self.valid_prefix.len() + self.hole_size;
+
         let valid_tail =
-            unsafe { slice::from_raw_parts(start.add(self.idx), self.old_len - self.idx) };
+            unsafe { slice::from_raw_parts(start.add(tail_start), self.old_len - tail_start) };
 
-        // SAFETY: `end - idx <= old_len - idx`, because `end <= old_len`. Also `idx <= end` by invariant.
         let (remainder, skipped_tail) =
-            unsafe { valid_tail.split_at_unchecked(self.end - self.idx) };
+            unsafe { valid_tail.split_at_unchecked(self.end - tail_start) };
 
         f.debug_struct("ExtractIf")
             .field("retained", &retained)
